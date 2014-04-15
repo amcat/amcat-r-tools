877670761c47309cb66b74a6e98ec273538b554a
kasperwelbers <kasperwelbers@gmail.com>
2014-04-15 02:41:18 +0200

    update demo_lda.r

--- demo_lda.r
+++ demo_lda.r
@@ -1,27 +1,36 @@
 setwd("~/projects/amcat-r-tools")
-source('../amcat-r/R/amcatr.r')
-source('../amcat-r/R/corpus.r')
+
+library(devtools)
+install_github("amcat/amcat-r")
 source('lda.r')
 
-conn = amcat.connect('http://amcat.vu.nl') # AmCAT vraagt om je inloggegevens
+conn = amcat.connect('http://preview.amcat.nl') # AmCAT vraagt om je inloggegevens
 
-articleset_id = 7927 # query "eu", 4 kranten, december 2013 (n = 370)
-features = amcat.features(conn, articleset_id)
+project_id = 6
+articleset_id = 45 
+tokens = amcat.gettokens(conn, project_id, articleset_id, page_size=10, module="tadpole")
 
-ldadata = lda.prepareFeatures(features, docfreq.thres=0, docfreq_pct.max=100, use.pos=c('noun','verb','NN'))
-m = lda.cluster(ldadata, nclusters=25, niterations=100)
-top.topic.words(m$topics)
+dtm = amcat.dtm.create(tokens$aid, tokens$lemma, tokens$freq)
+termstats = lda.vocabulary.statistics(dtm)
+voca = as.character(termstats[order(termstats$tfidf, decreasing=T),][1:10000,'term'])
+dtm = dtm[,voca]
+
+corpus = amcat.lda.fit(dtm)
+top.topic.words(corpus$topics)
+
+## visualize topics
+meta = amcat.articlemeta(conn, 10106)
+m = lda.addMeta(m, meta)
+
+amcat.plot.lda(corpus, path='/tmp/clouds/')
+
+
+
 
 # alternatief (topicmodels package)
 #dt = lda.dataframe_to_dtmatrix(features) # document-term matrix in sparsematrix structuur van tm
 #lda = LDA(dt, 25, LDAcontrol=list(iter=100))
 #terms(lda, 10) 
-
-
-## get meta for more fun ##
-
-meta = amcat.getMeta(conn, articleset_id)
-m = lda.addMeta(m, meta)
 
 ### select and name clusters
 labels = list("cluster1"=1,
--- lda.r
+++ lda.r
@@ -6,6 +6,11 @@ library(tm)
 library(wordcloud)
 library(RColorBrewer)
 
+lda.addMeta <- function(corpus, meta){
+  corpus = c(corpus, list(meta=meta[match(rownames(corpus$dtm), meta$id),]))
+  corpus
+}
+
 amcat.plot.lda <- function(corpus, path='/tmp/clouds/'){
   for(i in 1:nrow(corpus$document_sums)){
     print(paste('Plotting:',i))
@@ -58,103 +63,13 @@ amcat.plot.lda.wordcloud <- function(corpus, i){
   wordcloud(names, freqs, scale=c(6,.5),min.freq=1, max.words=Inf, random.order=FALSE, rot.per=.15, colors=pal)
 }
 
-lda.selectclusters <- function(model, selection) {
-  w = t(model$document_sums)
-  result = data.frame(rows=1:dim(w)[1])
-  select <- function(i) sum(w[i,selection[[n]]])
-  for (n in names(selection))  {
-    #print(n)
-    result = cbind(result, sapply(result$rows, select))   
-  }
-  result = result[,-1]
-  colnames(result) <- names(selection)
-  result
-}
-
-lda.getclusters <- function(clusters, labels, date=NULL, media=FALSE, aggfunc='sum', weight.length=F, column.pct=F, print.plot=F) { 
-  m = lda.selectclusters(clusters, labels)
-  if(weight.length == T) m = m / clusters$meta$length
-  break_by = list()
-  if(!is.null(date)){
-    if(date == 'day') d = as.Date(format(clusters$meta$date, '%Y-%m-%d'))
-    if(date == 'month') d = as.Date(paste(format(clusters$meta$date, '%Y-%m'),'-01',sep=''))
-    if(date == 'week') d = as.Date(paste(format(clusters$meta$date, '%Y-%W'),1), '%Y-%W %u')
-    if(date == 'year') d = as.Date(paste(format(clusters$meta$date, '%Y'),'-01-01',sep=''))
-    break_by = c(break_by, list(date=d))
-  } 
-  if(media == T) break_by = c(break_by, list(medium=clusters$meta$medium))
-  if(length(break_by) == 0) break_by = list(id=clusters$meta$id)
-  m = aggregate(m, by=break_by, FUN=aggfunc)
-  if(column.pct==T) for(column in names(labels)) m[,column] = m[,column] / sum(m[,column]) 
-  if(print.plot == T) lda.plot(m, date, media, aggfunc)
-  m
-}
-
-lda.plot <- function(m, date, media, aggfunc){
-  par(mfrow=c(1, 1))
-  plot.new()
-  if(media == T) {
-    meds = unique(m$medium)
-    par(mfrow=c((ceiling(length(meds)/3)+1), 3))
-    if(!is.null(date)) {
-      for(medium in meds) lda.plotdate(m$date[m$medium==medium], m[m$medium==medium,3:ncol(m)], aggfunc, medium, print.legend=F)
-      plot(1, type = "n", axes=FALSE, xlab="", ylab="")
-      clus = colnames(m[,3:ncol(m)])
-      legend(x = "top",inset = 0,
-             legend = clus, 
-             col=rainbow(length(clus)), lwd=5, cex=.8)
-    } else for(medium in meds) lda.plotbars(medium, m[m$medium==medium,2:ncol(m)], aggfunc)
-  } else {
-    if(!is.null(date)) {lda.plotdate(m$date, m[,2:ncol(m)], aggfunc, '')
-    } else lda.plotbars('', rowSums(m[,2:ncol(m)]), aggfunc='sum')
-  }
-}
-
-lda.plotdate <- function(x, lines, aggfunc, main_label, print.legend=T){
-  lines[is.na(lines)] = 0
-  x = x[order(x)]
-  lines = lines[order(x),]
-  if(print.legend==T) par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
-  plot(1, type="n", axes=F, xlab='Date', main=main_label, ylab=aggfunc,
-       xlim=c(min(x), max(x)), ylim=c(0, max(lines)), bty='L')
-  
-  axis(1, labels=x, at=x)
-  axis(2, las=1)
-  #box()
-  colors=rainbow(ncol(lines))
-  for (i in 1:ncol(lines)) {
-    lines(x=x, y=lines[,i], col=colors[i])
-  }
-  
-  if(print.legend == T) {
-    legend("topright", legend=colnames(lines), inset=c(-0.2,0), col=colors, cex=.5, pch = c(1,2), lty = c(1,2))
-    par(mar=c(5, 4, 4, 2) + 0.1)
-  }
-}
-
-lda.plotbars <- function(name, values, aggfunc){
-  colors=rainbow(ncol(values))
-  labels = colnames(values)
-  barplot(t(values)[,1], main=name, beside=TRUE,horiz=TRUE,
-          density=NA,
-          col=colors,
-          xlab=aggfunc,
-          ylab="",
-          axes=TRUE, names.arg=labels, cex.names=0.8, las=1)
-}
-
-chi2 <- function(a,b,c,d) {
-  # Compute the chi^2 statistic for a 2x2 crosstab containing the values
-  # [[a, b], [c, d]]
-  ooe <- function(o, e) {(o-e)*(o-e) / e}
-  tot = 0.0 + a+b+c+d
-  a = as.numeric(a)
-  b = as.numeric(b)
-  c = as.numeric(c)
-  d = as.numeric(d)
-  (ooe(a, (a+c)*(a+b)/tot)
-   +  ooe(b, (b+d)*(a+b)/tot)
-   +  ooe(c, (a+c)*(c+d)/tot)
-   +  ooe(d, (d+b)*(c+d)/tot))
+lda.vocabulary.statistics <- function(dt){
+  vocabulary = colnames(dt)
+  data.frame(term = vocabulary,
+             characters = nchar(vocabulary),
+             number = grepl("[0-9]", vocabulary),
+             termfreq = col_sums(dt),
+             docfreq = col_sums(dt > 0),
+             tfidf = tapply(dt$v/row_sums(dt)[dt$i], dt$j, mean) * log2(nDocs(dt)/col_sums(dt > 0)))
 }
 
