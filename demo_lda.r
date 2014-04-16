01164bb07ba84c3a63161c010246ae40ebed4027
kasperwelbers <kasperwelbers@gmail.com>
2014-04-16 12:03:26 +0200

    fixed minor issues

--- demo_lda.r
+++ demo_lda.r
@@ -1,8 +1,11 @@
 setwd("~/projects/amcat-r-tools")
-
-library(devtools)
-install_github("amcat/amcat-r")
+source('../amcat-r/R/corpus.r') # temporary solution till devtools gets fixed
+source('../amcat-r/R/amcatr.r')
 source('lda.r')
+
+#install.packages('devtools')
+#library(devtools)
+#install_github("amcat/amcat-r")
 
 conn = amcat.connect('http://preview.amcat.nl') # AmCAT vraagt om je inloggegevens
 
@@ -11,48 +14,16 @@ articleset_id = 45
 tokens = amcat.gettokens(conn, project_id, articleset_id, page_size=10, module="tadpole")
 
 dtm = amcat.dtm.create(tokens$aid, tokens$lemma, tokens$freq)
-termstats = lda.vocabulary.statistics(dtm)
+
+termstats = amcat.term.statistics(dtm)
 voca = as.character(termstats[order(termstats$tfidf, decreasing=T),][1:10000,'term'])
 dtm = dtm[,voca]
 
-corpus = amcat.lda.fit(dtm)
-top.topic.words(corpus$topics)
+m = amcat.lda.fit(dtm)
+top.topic.words(m$topics)
 
 ## visualize topics
-meta = amcat.articlemeta(conn, 10106)
-m = lda.addMeta(m, meta)
+meta = amcat.getarticlemeta(conn, articleset_id)
+m = amcat.lda.addMeta(m, meta)
 
-amcat.plot.lda(corpus, path='/tmp/clouds/')
-
-
-
-
-# alternatief (topicmodels package)
-#dt = lda.dataframe_to_dtmatrix(features) # document-term matrix in sparsematrix structuur van tm
-#lda = LDA(dt, 25, LDAcontrol=list(iter=100))
-#terms(lda, 10) 
-
-### select and name clusters
-labels = list("cluster1"=1,
-              "cluster2"=c(2,3),
-              "cluster3"=5)
-
-cluster.doc = lda.getclusters(m, labels) # niet geaggregeerd: waardes zijn aantal woorden per artikel (rij) dat aan cluster (kolom) toegekend is
-head(cluster.doc)
-
-# Artikelen met meer woorden (meta$length) hebben vanzelfsprekend een hogere score op clusters:
-# Daarom kan het nuttig zijn om hierop te wegen (maar niet altijd, want als we per tijdseenheid of medium kijken, dan kunnen we lange artikelen ook juist zwaarder willen laten wegen)
-cluster.doc = lda.getclusters(m, labels, weight.length=T) # op lengte wegen kan met weight.length=TRUE
-head(cluster.doc)
-
-# lda.getcluster kan ook aggregeren:
-lda.getclusters(m, labels, date='day') # aggregeer op datum, default is aggfunc='sum' (hier kunnen we ook wegen op length, maar dan zouden we korte en lange artikelen als even belangrijk zien, wat we niet willen denk ik)
-lda.getclusters(m, labels, date='year') # aggregeer op datum per 'day','week', 'month', of 'year'
-lda.getclusters(m, labels, media=T) # aggregeer op medium
-lda.getclusters(m, labels, date='week',media=T) # aggregeer op beide
-
-# met print.plot kun je dan ook direct plotten
-cluster.date = lda.getclusters(m, labels, date='week', print.plot=T)
-cluster.medium = lda.getclusters(m, labels, media=T, print.plot=T)
-cluster.datemed = lda.getclusters(m, labels, date='day', media=T, print.plot=T)
-
+amcat.plot.lda(m, date_interval='year', path='/tmp/clouds/')
--- lda.r
+++ lda.r
@@ -6,42 +6,44 @@ library(tm)
 library(wordcloud)
 library(RColorBrewer)
 
-lda.addMeta <- function(corpus, meta){
-  corpus = c(corpus, list(meta=meta[match(rownames(corpus$dtm), meta$id),]))
-  corpus
+amcat.lda.addMeta <- function(m, meta){
+  if('meta' %in% names(m)) {m$meta = meta[match(rownames(m$dtm), meta$id),]
+  } else m = c(m, list(meta=meta[match(rownames(m$dtm), meta$id),]))
+  m
 }
 
-amcat.plot.lda <- function(corpus, path='/tmp/clouds/'){
-  for(i in 1:nrow(corpus$document_sums)){
+amcat.plot.lda <- function(m, date_interval='year', path='/tmp/clouds/'){
+  for(i in 1:nrow(m$document_sums)){
     print(paste('Plotting:',i))
     fn = paste(path, i, ".png", sep="")
     if (!is.null(fn)) png(fn, width=1280,height=800)
     par(mar=c(4.5,4.5,2,2), cex.axis=1.7)
     layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE), widths=c(2.5,1.5), heights=c(1,2))
-    amcat.plot.lda.date(corpus, date, i)
-    amcat.plot.lda.wordcloud(corpus, i)
+    amcat.plot.lda.date(m, date_interval, i)
+    amcat.plot.lda.wordcloud(m, i)
     par(mar=c(11,4.5,2,6))
-    amcat.plot.lda.medium(corpus, i)
+    amcat.plot.lda.medium(m, i)
     if (!is.null(fn)) dev.off()
   }
 }
 
-amcat.plot.lda.date <- function(corpus, date, i, pct=T){
-  if(date == 'day') d = as.Date(format(corpus$meta$date, '%Y-%m-%d'))
-  if(date == 'month') d = as.Date(paste(format(corpus$meta$date, '%Y-%m'),'-01',sep=''))
-  if(date == 'week') d = as.Date(paste(format(corpus$meta$date, '%Y-%W'),1), '%Y-%W %u')
-  if(date == 'year') d = as.Date(paste(format(corpus$meta$date, '%Y'),'-01-01',sep=''))
+
+amcat.plot.lda.date <- function(m, date_interval, i, pct=T){
+  if(date_interval == 'day') d = as.Date(format(m$meta$date, '%Y-%m-%d'))
+  if(date_interval == 'month') d = as.Date(paste(format(m$meta$date, '%Y-%m'),'-01',sep=''))
+  if(date_interval == 'week') d = as.Date(paste(format(m$meta$date, '%Y-%W'),1), '%Y-%W %u')
+  if(date_interval == 'year') d = as.Date(paste(format(m$meta$date, '%Y'),'-01-01',sep=''))
   
-  values = corpus$document_sums[i,]
+  values = m$document_sums[i,]
   if(pct == T) values = values / sum(values)
   agg = aggregate(values, by=list(date=d), FUN='sum')
   plot(agg$date, agg$x, type='l', xlab='', main='', ylab='',
        xlim=c(min(agg$date), max(agg$date)), ylim=c(0, max(agg$x)), bty='L', lwd=5)
 }
 
-amcat.plot.lda.medium <- function(corpus, i, pct=T){
-  medium = corpus$meta$medium
-  values = corpus$document_sums[i,]
+amcat.plot.lda.medium <- function(m, i, pct=T){
+  medium = m$meta$medium
+  values = m$document_sums[i,]
   if(pct == T) values = values / sum(values)
   agg = aggregate(values, by=list(medium=medium), FUN='sum')
   colors=rainbow(nrow(agg))
@@ -54,22 +56,14 @@ amcat.plot.lda.medium <- function(corpus, i, pct=T){
 }
 
 
-amcat.plot.lda.wordcloud <- function(corpus, i){
-  x = corpus$topics[i,]
+amcat.plot.lda.wordcloud <- function(m, i){
+  x = m$topics[i,]
   x = sort(x[x>5], decreasing=T)[1:100]
+  x = x[!is.na(x)]
   names = sub("/.*", "", names(x))
   freqs = x**.5
   pal <- brewer.pal(6,"YlGnBu")
-  wordcloud(names, freqs, scale=c(6,.5),min.freq=1, max.words=Inf, random.order=FALSE, rot.per=.15, colors=pal)
+  wordcloud(names, freqs, scale=c(6,.5), min.freq=1, max.words=Inf, random.order=FALSE, rot.per=.15, colors=pal)
 }
 
-lda.vocabulary.statistics <- function(dt){
-  vocabulary = colnames(dt)
-  data.frame(term = vocabulary,
-             characters = nchar(vocabulary),
-             number = grepl("[0-9]", vocabulary),
-             termfreq = col_sums(dt),
-             docfreq = col_sums(dt > 0),
-             tfidf = tapply(dt$v/row_sums(dt)[dt$i], dt$j, mean) * log2(nDocs(dt)/col_sums(dt > 0)))
-}
 
