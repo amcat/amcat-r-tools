2556c9616824be0a4e93efd606531886473385e6
kasperwelbers <kasperwelbers@gmail.com>
2014-05-06 17:45:46 +0200

    some cleanup of the lda plotting syntax and added some graph function to lda.r

--- demo_lda.r
+++ demo_lda.r
@@ -1,22 +1,22 @@
-setwd("~/projects/amcat-r-tools")
-source('../amcat-r/R/corpus.r') # temporary solution till devtools gets fixed
-source('../amcat-r/R/amcatr.r')
-source('lda.r')
-
+## First install the amcat-r package from github
 #install.packages('devtools')
 #library(devtools)
-#install_github("amcat/amcat-r")
+#install_github("amcat/amcat-r") 
+
+library(amcatr)
+source('lda.r') # from amcat-r-tools folder
 
 conn = amcat.connect('http://preview.amcat.nl') # AmCAT vraagt om je inloggegevens
 
-project_id = 6
-articleset_id = 45 
-tokens = amcat.gettokens(conn, project_id, articleset_id, page_size=10, module="tadpole")
+project_id = 403
+articleset_id = 10277 # 1083 english articles about the Rwandan genocide in The Guardian, NYT and The Toronto Star
+tokens = amcat.gettokens(conn, project_id, articleset_id, page_size=10, module="corenlp_lemmatize")
 
 dtm = amcat.dtm.create(tokens$aid, tokens$lemma, tokens$freq)
 
 termstats = amcat.term.statistics(dtm)
-voca = as.character(termstats[order(termstats$tfidf, decreasing=T),][1:10000,'term'])
+termstats = termstats[termstats$termfreq > 2 & termstats$nonalpha == F,]
+voca = as.character(termstats[order(termstats$tfidf, decreasing=T),][1:8000,'term'])
 dtm = dtm[,voca]
 
 m = amcat.lda.fit(dtm)
@@ -26,10 +26,17 @@ top.topic.words(m$topics)
 meta = amcat.getarticlemeta(conn, articleset_id)
 m = amcat.lda.addMeta(m, meta)
 
-amcat.plot.lda(m, date_interval='year', path='/tmp/clouds/')
+## visualize topics
+par(mar=c(5,3,3,3))
+amcat.plot.lda.wordcloud(m, topic_nr=1)
+amcat.plot.lda.time(m, topic_nr=1, date_interval='week') # default time variable is m$meta$date 
+amcat.plot.lda.category(m, 1) # default category variable is m$meta$medium
+amcat.plot.lda.topic(m, 1)
 
+# print topic plots for all topics
+amcat.plot.lda(m, date_interval='day', path='/tmp/clouds/') ## set path to existing directory
 
 ## in progress
 ## Plot model fit for different values of K (takes a long time as it calculats a model for every value in k_values)
-source('lda_ktests.r')
-fit_scores = amcat.lda.find.best.K(dtm, k_values=seq(2,100,10))
+#source('lda_ktests.r')
+#fit_scores = amcat.lda.find.best.K(dtm, k_values=seq(2,100,10))
--- lda.r
+++ lda.r
@@ -1,8 +1,4 @@
-library(lda)
 library(reshape)
-library(topicmodels)
-library(slam)
-library(tm)
 library(wordcloud)
 library(RColorBrewer)
 
@@ -12,58 +8,141 @@ amcat.lda.addMeta <- function(m, meta){
   m
 }
 
-amcat.plot.lda <- function(m, date_interval='year', path='/tmp/clouds/'){
-  for(i in 1:nrow(m$document_sums)){
-    print(paste('Plotting:',i))
-    fn = paste(path, i, ".png", sep="")
+## PLOTTING
+amcat.plot.lda.alltopics <- function(m, time_var=m$meta$date, category_var=m$meta$medium, date_interval='day', path='/tmp/clouds/'){
+  for(topic_nr in 1:nrow(m$document_sums)){
+    print(paste('Plotting:',topic_nr))
+    fn = paste(path, topic_nr, ".png", sep="")
     if (!is.null(fn)) png(fn, width=1280,height=800)
-    par(mar=c(4.5,4.5,2,2), cex.axis=1.7)
-    layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE), widths=c(2.5,1.5), heights=c(1,2))
-    amcat.plot.lda.date(m, date_interval, i)
-    amcat.plot.lda.wordcloud(m, i)
-    par(mar=c(11,4.5,2,6))
-    amcat.plot.lda.medium(m, i)
+    amcat.plot.lda.topic(m, topic_nr, time_var, category_var, date_interval)
     if (!is.null(fn)) dev.off()
   }
+  par(mfrow=c(1,1))
 }
 
-
-amcat.plot.lda.date <- function(m, date_interval, i, pct=T){
-  if(date_interval == 'day') d = as.Date(format(m$meta$date, '%Y-%m-%d'))
-  if(date_interval == 'month') d = as.Date(paste(format(m$meta$date, '%Y-%m'),'-01',sep=''))
-  if(date_interval == 'week') d = as.Date(paste(format(m$meta$date, '%Y-%W'),1), '%Y-%W %u')
-  if(date_interval == 'year') d = as.Date(paste(format(m$meta$date, '%Y'),'-01-01',sep=''))
-  
-  values = m$document_sums[i,]
-  if(pct == T) values = values / sum(values)
-  agg = aggregate(values, by=list(date=d), FUN='sum')
-  plot(agg$date, agg$x, type='l', xlab='', main='', ylab='',
-       xlim=c(min(agg$date), max(agg$date)), ylim=c(0, max(agg$x)), bty='L', lwd=5)
+amcat.plot.lda.topic <- function(m, topic_nr, time_var=m$meta$date, category_var=m$meta$medium, date_interval='day', pct=F, value='total'){
+  par(mar=c(4.5,3,2,1), cex.axis=1.7)
+  layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE), widths=c(2.5,1.5), heights=c(1,2))
+  amcat.plot.lda.time(m, topic_nr, time_var, date_interval, pct=pct, value=value)
+  amcat.plot.lda.wordcloud(m, topic_nr)
+  amcat.plot.lda.category(m, topic_nr, category_var, pct=pct, value=value)
+  par(mfrow=c(1,1))
 }
 
-amcat.plot.lda.medium <- function(m, i, pct=T){
-  medium = m$meta$medium
-  values = m$document_sums[i,]
-  if(pct == T) values = values / sum(values)
-  agg = aggregate(values, by=list(medium=medium), FUN='sum')
-  colors=rainbow(nrow(agg))
-  barplot(t(agg$x), main='', beside=TRUE,horiz=FALSE,
+amcat.prepare.time.var <- function(time_var, date_interval){
+  if(class(time_var) == 'Date'){
+    if(date_interval == 'day') time_var = as.Date(format(time_var, '%Y-%m-%d'))
+    if(date_interval == 'month') time_var = as.Date(paste(format(time_var, '%Y-%m'),'-01',sep=''))
+    if(date_interval == 'week') time_var = as.Date(paste(format(time_var, '%Y-%W'),1), '%Y-%W %u')
+    if(date_interval == 'year') time_var = as.Date(paste(format(time_var, '%Y'),'-01-01',sep=''))
+  } 
+  time_var
+}
+
+amcat.fill.time.gaps <- function(d, date_interval){
+  if(class(d$time) == 'numeric'){
+    for(t in min(d$time):max(d$time)) 
+      if(!t %in% d$time) d = rbind(d, data.frame(time=t, value=0))
+  }
+  if(class(d$time) == 'Date'){
+    date_sequence = seq.Date(from=min(d$time), to=max(d$time), by=date_interval)
+    for(i in 1:length(date_sequence)){
+      t = date_sequence[i]
+      if(!t %in% d$time) d = rbind(d, data.frame(time=t, value=0))
+    }
+  }
+  d[order(d$time),]
+}
+
+amcat.prepare.plot.values <- function(m, break_var, topic_nr, pct=F, value='total'){
+  d = data.frame(value=m$document_sums[topic_nr,], break_var=break_var)
+  if(value == 'relative') d$value= d$value / colSums(m$document_sums)
+  if(pct == T) d$value = d$value / sum(d$value)
+  d = aggregate(d[,c('value')], by=list(break_var=d$break_var), FUN='sum')  
+  d
+}
+
+amcat.plot.lda.time <- function(m, topic_nr, time_var=m$meta$date, date_interval='day', pct=F, value='total'){
+  par(mar=c(3,3,3,1))
+  time_var = amcat.prepare.time.var(time_var, date_interval)  
+  d = amcat.prepare.plot.values(m, break_var=time_var, topic_nr=topic_nr, pct=pct, value=value)
+  colnames(d) = c('time','value')
+  d = amcat.fill.time.gaps(d, date_interval)
+  plot(d$time, d$value, type='l', xlab='', main='', ylab='', xlim=c(min(d$time), max(d$time)), ylim=c(0, max(d$value)), bty='L', lwd=5, col='darkgrey')
+  d
+}
+
+amcat.plot.lda.category <- function(m, topic_nr, category_var=m$meta$medium, pct=F, value='total'){
+  par(mar=c(10,0,1,2))
+  d = amcat.prepare.plot.values(m, break_var=category_var, topic_nr=topic_nr, pct=pct, value=value)
+  colnames(d) = c('category','value')
+  barplot(as.matrix(t(d[,c('value')])), main='', beside=TRUE,horiz=FALSE,
           density=NA,
-          col='lightgrey',
+          col='darkgrey',
           xlab='',
           ylab="",
-          axes=TRUE, names.arg=agg$medium, cex.names=1.5, cex.axis=1.5, adj=1, las=2)
+          axes=T, names.arg=d$category, cex.names=1.5, cex.axis=1.5, adj=1, las=2)
+  d
 }
 
-
-amcat.plot.lda.wordcloud <- function(m, i){
-  x = m$topics[i,]
+amcat.plot.lda.wordcloud <- function(m, topic_nr){
+  x = m$topics[topic_nr,]
   x = sort(x[x>5], decreasing=T)[1:100]
   x = x[!is.na(x)]
   names = sub("/.*", "", names(x))
   freqs = x**.5
   pal <- brewer.pal(6,"YlGnBu")
   wordcloud(names, freqs, scale=c(6,.5), min.freq=1, max.words=Inf, random.order=FALSE, rot.per=.15, colors=pal)
+}
+
+## GRAPHS
+
+amcat.ucmatrix.to.simmatrix<- function(ucmatrix, similarity.measure){
+  if(similarity.measure=='pearson_cor') sim.matrix = cor(t(ucmatrix))
+  if(similarity.measure=='spearman_cor') sim.matrix = cor(t(ucmatrix), method='spearman')
+  if(similarity.measure=='hellinger_dist') {
+    library(topicmodels)
+    sim.matrix = distHellinger(as.matrix(ucmatrix))
+  }
+  if(similarity.measure=='cosine') {
+    library(lsa)
+    sim.matrix = cosine(t(as.matrix(ucmatrix))) 
+  }
+  sim.matrix
+}
+
+amcat.simmatrix.to.simgraph <- function(m){
+  m[lower.tri(m, diag=T)] = NA
+  m.indices = which(!is.na(m),arr.ind=T)
+  m.values = na.omit(as.vector(m))
+  data.frame(node.X=m.indices[,1], node.Y=m.indices[,2], similarity=m.values)
+}
+
+amcat.ucmatrix.to.simgraph <- function(ucmatrix, similarity.measure='pearson_cor'){
+  sim = amcat.ucmatrix.to.simmatrix(ucmatrix, similarity.measure)
+  amcat.simmatrix.to.simgraph(sim)
+}
+
+amcat.unit.similarity.graph <- function(ucmatrix, unit.id.list, similarity.measure='pearson_cor'){
+  d = aggregate(ucmatrix, by=unit.id.list, FUN='sum')
+  ucmatrix = d[,(length(unit.id.list)+1):ncol(d)]
+  simgraph = amcat.ucmatrix.to.simgraph(ucmatrix, similarity.measure)
+  
+  meta = as.data.frame(d[,names(unit.id.list)], stringsAsFactors=F)
+  colnames(meta) = names(unit.id.list)
+  meta = cbind(id=1:nrow(meta), meta)
+  meta$topic_totals = rowSums(ucmatrix)
+  
+  list(graph_df=simgraph, meta=meta)
+}
+
+amcat.lda.similarity.graph <- function(m, unit.id.list, vertex_label=names(unit.id.list)[1], similarity.measure='pearson_cor'){
+  library(igraph)
+  doc_topic_matrix = t(m$document_sums)
+  unit.similarity = amcat.unit.similarity.graph(doc_topic_matrix, unit.id.list, similarity.measure)
+  g = graph.data.frame(unit.similarity$graph_df, directed=F, vertices=unit.similarity$meta)
+  E(g)$weight = E(g)$similarity
+  g
 }
 
 
